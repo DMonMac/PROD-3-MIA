e9f63d9c397d4e34fd4b9f8250c29d29cec6deff
DMonMac <dmmacaso@gmail.com>
2017-11-16 14:16:20 +0800

    Returned to rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
 
